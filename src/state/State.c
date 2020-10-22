21495fdcaf665db7ac070367992d3bd05064b634
Josh Lengel <jlengel12@gmail.com>
2020-10-22 22:12:19 +0200

    Moved timer reset to after the start of next state

--- src/state/State.c
+++ src/state/State.c
@@ -18,12 +18,13 @@ IC_BOOL __state_check_exit_impl(State_Machine *state_machine)
 
         if (next)
         {
-            timer_start(current->timer);
-
             next->window = current->window;
             next->input = current->input;
             next->timer = current->timer;
             next->start_proc(next);
+
+            timer_start(current->timer);
+            
             state_machine->current_state = next;
 
             if (next->exit) return IC_TRUE;
